86fb01c2e71f7c2e97b37ecd04a60fa380b4ea56
poobalan <mrpoobalan23@gmail.com>
2015-10-15 21:20:56 +0530

    config set baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = "http://localhost/facebook-google-login-master";
+$config['base_url'] = "http://localhost/google";
 
 /*
 |--------------------------------------------------------------------------
